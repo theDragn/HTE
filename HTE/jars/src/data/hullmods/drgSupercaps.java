f91de4fc705b51d8a93ca45335114b86d09e12e9
theDragn <61987648+theDragn@users.noreply.github.com>
2020-12-19 21:25:56 -0500

    Initial upload

---
+++ HTE/jars/src/data/hullmods/drgSupercaps.java
@@ -0,0 +1,27 @@
+package data.hullmods;
+
+import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+
+public class drgSupercaps extends BaseHullMod {
+
+	public drgSupercaps() {
+        // bitcoin miner code goes here
+    }
+        
+    public static float
+        ENERGY_WEP_DAMAGE_BONUS = 75,
+        ENERGY_WEP_FLUX_BONUS = 25;
+	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
+		stats.getEnergyWeaponDamageMult().modifyPercent(id, ENERGY_WEP_DAMAGE_BONUS);
+		stats.getEnergyWeaponFluxCostMod().modifyPercent(id, ENERGY_WEP_FLUX_BONUS);
+	}
+
+    public String getDescriptionParam(int index, HullSize hullSize) {
+        if (index == 0) return (int)ENERGY_WEP_DAMAGE_BONUS + "%";
+        if (index == 1) return (int)ENERGY_WEP_FLUX_BONUS + "%";
+        return null;
+    }
+
+}
